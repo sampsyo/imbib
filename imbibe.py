131a416d07499eff7a7e3519bf85cdfb2d27ae2e
Adrian Sampson <adrian@radbox.org>
2016-05-30 12:04:04 -0700

    ACM scraper

--- imbibe.py
+++ imbibe.py
@@ -3,15 +3,50 @@
 import requests
 import click
 import re
+from urllib.parse import urlparse, parse_qs
+import bs4
 
 CITE_RE = r'^\[@([^\]]+)\]:\s*(.*)$'
+
+
+def fetch_acm(acm_id):
+    req = requests.get(
+        'http://dl.acm.org/exportformats.cfm',
+        params={
+            'id': acm_id,
+            'expformat': 'bibtex',
+        },
+    )
+    soup = bs4.BeautifulSoup(req.content, 'html.parser')
+    for pre in soup.find_all('pre'):
+        bib_entry = pre.get_text().strip()
+        print(bib_entry)
+
+
+def scrape_acm(url):
+    # Check whether this is an ACM citation.
+    parts = urlparse(url)
+    if parts.netloc == 'dl.acm.org' and parts.path == '/citation.cfm':
+        query = parse_qs(parts.query)
+        if 'id' in query:
+            cite_id = query['id'][0]
+            return fetch_acm(cite_id)
+    return None
+
+
+SCRAPERS = [scrape_acm]
+
+
+def url_to_bib(url):
+    for scrape in SCRAPERS:
+        scrape(url)
 
 
 def convert(in_stream, out_stream):
     in_text = in_stream.read()
     for match in re.finditer(CITE_RE, in_text):
         key, url = match.groups()
-        print(key, url)
+        print(key, url_to_bib(url))
 
 
 @click.command()
